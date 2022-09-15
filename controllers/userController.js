9e55f2f2a854b5bbc055377408f8faef1edb6d80
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-09-16 01:45:35 +0600

    basic get & post method setup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,20 @@
+const users = require("../users.json");
+const fs = require("fs");
+
 module.exports.getAllUsers = (req, res, next) => {
-  res.send("All users from CONTROLLER");
+  res.send(users);
+};
+module.exports.getUserById = (req, res, next) => {
+  const { id } = req.params;
+  const findUser = users.find((user) => user.id === Number(id));
+  res.send(findUser);
 };
 
 module.exports.addUser = (req, res) => {
-  res.send("Added user from CONTROLLER");
+  console.log(users);
+  const newUser = req.body;
+  // console.log(newUser);
+  users.push(newUser);
+  console.log(users);
+  res.send("Added new user!!!");
 };
--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const userRouters = require("./routes/v1/userRoutes");
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.use("/api/v1/users", userRouters);
+app.use("/api/v1/user", userRouters);
 
 // if not match all types of HTTP request or existing routes
 app.all("*", (req, res) => {
